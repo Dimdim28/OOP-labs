a0edbdfdc9028d9e8b54b655611ae3c6df3b3b9c
Dimdim28 <wwwdimanes1@gmail.com>
2022-12-10 16:29:36 +0200

    Added 5 lab

---
+++ 5/added deleting/lab5/Lab5/lineoo_shape.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "line_shape.h"
+#include "ellipse_shape.h"
+
+class LineOOShape : public LineShape, public EllipseShape
+{
+public:
+	LineOOShape(void);
+	Shape* copyShape();
+
+	std::wstring getName();
+	void Draw(HDC);
+	void Gum(HDC);
+};
